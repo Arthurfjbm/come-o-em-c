f9f01e14f1644190f5da3868fe04594bf50a1eed
Arthurfjbm <arthurfjbm@gmail.com>
2025-04-17 20:38:11 -0300

    Create aula5_dia_da_semana.c

---
+++ aula5_dia_da_semana.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(){
+
+int day ;
+
+printf("digite o numero correspondente ao dia : ");
+scanf("%d", day);
+
+
+
+switch (day) {
+  case 1:
+    printf("Monday");
+    break;
+  case 2:
+    printf("Tuesday");
+    break;
+  case 3:
+    printf("Wednesday");
+    break;
+  case 4:
+    printf("Thursday");
+    break;
+  case 5:
+    printf("Friday");
+    break;
+  case 6:
+    printf("Saturday");
+    break;
+  case 7:
+    printf("Sunday");
+    break;  
+  default:
+    printf( "numero invalido");
+}
+
+return 0;
+}
+
