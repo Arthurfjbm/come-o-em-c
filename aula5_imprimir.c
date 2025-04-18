95be8e295c8a7e0c3f97fcd4d3b84027df17cbb4
Arthurfjbm <arthurfjbm@gmail.com>
2025-04-17 21:03:07 -0300

    Create aula5_imprimir.c

---
+++ aula5_imprimir.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    
+    for(i=1; i<11; i++) {
+        printf("%d\t", i);
+    }
+
+    printf("\n");
+    
+    for(i=10; i>0; i--) {
+        printf("%d\t", i);
+    }
+
+return 0;
+}
